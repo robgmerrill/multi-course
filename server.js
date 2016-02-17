c1261b53d397c57cef7690753195504957573430
Rob Merrill <robgmerrill@gmail.com>
2016-02-17 13:36:04 -0800

    mongo update and deployment ready

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
   stylus = require('stylus'),
   logger = require('morgan'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -24,53 +25,37 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
+if(env === 'development') {
+  mongoose.connect('mongodb://localhost/multicourse');
+} else {
+mongoose.connect('mongodb://robgmerrill:multicourse@ds011278.mongolab.com:11278/multicourse');
+}
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('multicourse db opened');
+});
+var messageSchema = mongoose.Schema({
+  message: String
+});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
+});
+
+
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
 });
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
 
-// var express = require('express'), 
-//     stylus = require('stylus'),
-//     logger = require('morgan'),
-//     bodyParser = require('body-parser');
-
-// var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-// var app = express();
-
-// function compile(str, path) {
-//   return stylus(str).set('filename', path);
-// }
-
-// app.set('views', __dirname + '/server/views');
-// app.set('view engine', 'jade');
-// app.use(logger('dev'));
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
-// app.use(stylus.middleware(
-// {
-//   src:__dirname + '/public', 
-//   compile: compile
-// }
-//   ));
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/partials/:partialPath', function(req, res) {
-//     console.log('rendering partial', '/partials/' + req.params.partialPath);
-//     res.render('partials/' + req.params.partialPath);
-// });
-
-// app.get('*', function(req, res) {
-//   res.render('index');
-// });
-
-// var port = 3000;
-// app.listen(port);
-// console.log('Listening on port ' + port);

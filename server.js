e28a3e8240611cc688952b362e7cf8abab97cde0
Rob Merrill <robgmerrill@gmail.com>
2016-02-17 12:09:58 -0800

    angular added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express = require('express'), 
-    stylus = require('stylus'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser');
+var express = require('express'),
+  stylus = require('stylus'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -14,20 +14,63 @@ function compile(str, path) {
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(stylus.middleware(
-{
-  src:__dirname + '/public', 
-  compile: compile
-}
-  ));
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
 app.use(express.static(__dirname + '/public'));
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
 
 app.get('*', function(req, res) {
   res.render('index');
 });
 
-var port = 3000;
+var port = 3030;
 app.listen(port);
-console.log('Listening on port ' + port);
+console.log('Listening on port ' + port + '...');
+
+// var express = require('express'), 
+//     stylus = require('stylus'),
+//     logger = require('morgan'),
+//     bodyParser = require('body-parser');
+
+// var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// var app = express();
+
+// function compile(str, path) {
+//   return stylus(str).set('filename', path);
+// }
+
+// app.set('views', __dirname + '/server/views');
+// app.set('view engine', 'jade');
+// app.use(logger('dev'));
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(stylus.middleware(
+// {
+//   src:__dirname + '/public', 
+//   compile: compile
+// }
+//   ));
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/partials/:partialPath', function(req, res) {
+//     console.log('rendering partial', '/partials/' + req.params.partialPath);
+//     res.render('partials/' + req.params.partialPath);
+// });
+
+// app.get('*', function(req, res) {
+//   res.render('index');
+// });
+
+// var port = 3000;
+// app.listen(port);
+// console.log('Listening on port ' + port);

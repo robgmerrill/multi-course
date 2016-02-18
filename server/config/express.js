ff6539af9991121b3a48fcbb3b41a349f7c55d65
Rob Merrill <robgmerrill@gmail.com>
2016-02-17 16:47:59 -0800

    created client side login

--- server/config/express.js
+++ server/config/express.js
@@ -1,7 +1,10 @@
 var express = require('express'),
   logger = require('morgan'),
   bodyParser = require('body-parser'),
-  stylus = require('stylus');
+  stylus = require('stylus'), 
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  passport = require('passport');
 
 module.exports = function(app, config) {
 function compile(str, path) {
@@ -11,8 +14,12 @@ function compile(str, path) {
 app.set('views', config.rootPath + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(session({secret: 'multi course dog',resave:false, saveUninitialized:false}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(stylus.middleware(
   {
     src: config.rootPath + '/public',
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,5 +7,22 @@ module.exports = function(config) {
   db.once('open', function callback() {
     console.log('multicourse db opened');
   });
+
+  var userSchema = mongoose.Schema({
+    firstName: String, 
+    lastName: String,
+    userName: String
+  });
+  var User = mongoose.model('User', userSchema);
+
+  User.find({}).exec(function(err, collection) {
+    if(collection.length === 0) {
+      User.create({firstName: 'Rob', lastName: 'Merrill', userName: 'Rob'});
+      User.create({firstName: 'Marissa', lastName: 'Merrill', userName: 'Marissa'});
+      User.create({firstName: 'Oso', lastName: 'Merrill', userName: 'Oso'});
+      User.create({firstName: 'Tom', lastName: 'Merrill', userName: 'Tom'});
+    }
+  })
+
 }
 
